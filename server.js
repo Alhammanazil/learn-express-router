1cd227ae88d1f8b531bffa81fe13133c181ddc91
alhammanazil <alhammanazil18@gmail.com>
2024-02-15 13:51:30 +0700

    make sealed cookies

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ const port = 3000;
 // Define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser('secret-key'));
 
 // Define routes
+app.get('/signingcookie', (req, res) => {
+    res.cookie('paket', 'ransel', { signed: true });
+    res.send('Cookie has been signed');
+});
+
+app.get('/verifycookie', (req, res) => {
+    const cookies = req.signedCookies;
+    res.send(cookies);
+});
+
 app.use('/theater', require('./routes/theater'));
 app.use('/movies', require('./routes/movies'));
 app.use('/admin', require('./routes/admin'));
