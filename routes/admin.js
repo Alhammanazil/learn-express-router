ef2f939aab526dd520df6d4db19617043b8d4961
alhammanazil <alhammanazil18@gmail.com>
2024-02-16 13:58:19 +0700

    session implementation in express.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const cookieSignature = require('cookie-signature');
 const router = express.Router();
 
 // Define middleware
 router.use((req, res, next) => {
     if (req.query.isAdmin) {
         next();
+    } else {
+        res.status(401).send('Unauthorized');
     }
-    res.status(401).send('Unauthorized');
 });
 
 // Define routes
 router.get('/', (req, res) => {
-    res.cookie('token', '12345')
-    res.cookie('username', 'admin')
+    const token = '12345';
+    const secretKey = 'secret-key'; // Ganti dengan kunci rahasia Anda
+    
+    // Menggunakan cookie-signature untuk menandatangani token
+    const signedToken = cookieSignature.sign(token, secretKey);
+    
+    res.cookie('token', signedToken);
+    res.cookie('username', 'admin');
     res.send('Welcome to the admin panel');
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
+const session = require('express-session');
+
+// Define port
 const port = 3000;
 
 // Define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser('secret-key'));
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 // Define routes
 app.get('/signingcookie', (req, res) => {
     res.cookie('paket', 'ransel', { signed: true });
     res.send('Cookie has been signed');
+});
+
+app.get('/count', (req, res) => {
+    if (req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`Count: ${req.session.count}`);
 });
 
 app.get('/verifycookie', (req, res) => {
