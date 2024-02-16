189f40fbc2d9a4e6b7130b925fe631be97488b5c
alhammanazil <alhammanazil18@gmail.com>
2024-02-16 14:16:34 +0700

    another example of session use

--- server.js
+++ server.js
@@ -14,10 +14,13 @@ app.use(session({
     secret: 'secret-key',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true }
+    cookie: { secure: false }
 }));
 
 // Define routes
+app.use('/theater', require('./routes/theater'));
+app.use('/movies', require('./routes/movies'));
+app.use('/admin', require('./routes/admin'));
 app.get('/signingcookie', (req, res) => {
     res.cookie('paket', 'ransel', { signed: true });
     res.send('Cookie has been signed');
@@ -37,9 +40,23 @@ app.get('/verifycookie', (req, res) => {
     res.send(cookies);
 });
 
-app.use('/theater', require('./routes/theater'));
-app.use('/movies', require('./routes/movies'));
-app.use('/admin', require('./routes/admin'));
+app.get('/register', (req, res) => {
+    const { username } = req.query;
+    if (username) {
+        req.session.username = username;
+        res.redirect('/dashboard');
+    } else {
+        res.send('Register failed, please provide a username');
+    }
+});
+
+app.get('/dashboard', (req, res) => {
+    if (req.session.username) {
+        res.send(`Welcome to dashboard ${req.session.username} 👋`);
+    } else {
+        res.send('You need to login first');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
